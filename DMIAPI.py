4f867c1149c0b38ba06c4d2be52ac1be0321016e
Verdoner <jx55oc@student.aau.dk>
2025-02-25 09:03:17 +0100

    Played with Plotting

--- DMIAPI.py
+++ DMIAPI.py
@@ -7,7 +7,7 @@ load_dotenv()
 DMIAPIKEY = os.getenv("DMIAPIKEY")
 
 def get_temp():
-    url = 'https://dmigw.govcloud.dk/v2/climateData/collections/municipalityValue/items?municipalityId=0851&api-key='+DMIAPIKEY+'&timeResolution=hour&parameterId=mean_temp'
+    url = 'https://dmigw.govcloud.dk/v2/climateData/collections/municipalityValue/items?municipalityId=0851&api-key='+DMIAPIKEY+'&timeResolution=hour&parameterId=mean_temp&limit=24'
     r = requests.get(url)
     data = json.loads(r.text)
     temp_map = {}
@@ -16,4 +16,20 @@ def get_temp():
     return temp_map
 
 
-print(get_temp())
+#print(get_temp())
+
+import plotly.express as px
+import pandas as pd
+
+data = get_temp()
+
+df = pd.DataFrame(list(data.items()), columns=["Time", "Value"])
+
+# Create an interactive plot
+fig = px.line(df, x="Time", y="Value", title="Time-Series Data (Plotly)")
+
+
+fig.update_xaxes(title="Time", tickformat="%m-%d %H:%M", tickangle=45, dtick="D1")
+fig.update_yaxes(title="Value")
+
+fig.show()
