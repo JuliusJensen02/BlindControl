a1d82a7c74fdaa4ae95e71277939928e278a4d58
JuliusJensen02 <113088719+JuliusJensen02@users.noreply.github.com>
2025-03-03 18:05:37 +0100

    Trying new parameters

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from scripts.greybox_fitting import get_constants, predict_for_date
 
-constants = get_constants("2024-11-24T00:00:00Z", 30)
+constants = get_constants("2024-11-25T00:00:00Z", 1)
 
-predict_for_date("2025-02-11T00:00:00Z", constants, True)
+predict_for_date("2024-11-25T00:00:00Z", constants, True)
--- scripts/greybox_fitting.py
+++ scripts/greybox_fitting.py
@@ -1,4 +1,5 @@
 import csv
+import os
 from datetime import datetime, timedelta
 
 import numpy as np
@@ -9,10 +10,10 @@ from scripts.data_processing import convert_csv_to_df, remove_outliers, smooth
 from scripts.data_to_csv import reset_csv, query_data
 from scripts.plot import plot_df
 
-t_r, t_a, s = [[], [], []]
+t_r, t_a, s, h_sp = [[], [], [], []]
 
 def predict_for_date(start_time, constants, plot):
-    global t_r, t_a, s
+    global t_r, t_a, s, h_sp, v
     reset_csv()
     query_data(start_time, 1)
     df = convert_csv_to_df("data/data.csv")
@@ -20,6 +21,7 @@ def predict_for_date(start_time, constants, plot):
     t_r = df["room_temp"].values
     t_a = df["ambient_temp"].values
     s = df["watt"].values
+    h_sp = df["heating_setpoint"].values
     df['temp_predictions'] = predict_temperature(list(constants.values()))
     if plot:
         plot_df(df)
@@ -27,14 +29,18 @@ def predict_for_date(start_time, constants, plot):
     return df
 
 def get_constants(start_time = "2025-01-01T00:00:00Z", days = 1):
-    df = convert_csv_to_df("data/constants_cache.csv")
-    if len(df) == 0 or start_time != df['start_time'][0] or days != df['days'][0]:
+    if os.path.getsize("data/constants_cache.csv") == 0:
         train_for_time_frame(start_time, days)
         df = convert_csv_to_df("data/constants_cache.csv")
+    else:
+        df = convert_csv_to_df("data/constants_cache.csv")
+        if len(df) == 0 or start_time != df['start_time'][0] or days != df['days'][0]:
+            train_for_time_frame(start_time, days)
+            df = convert_csv_to_df("data/constants_cache.csv")
     return {'alpha_a': df['alpha_a'], 'alpha_s': df['alpha_s'], 'beta_r': df['beta_r'], 'beta_v': df['beta_v']}
 
 def train_for_time_frame(start_time = "2025-01-01T00:00:00Z", days = 1):
-    global t_r, t_a, s
+    global t_r, t_a, s, h_sp
     time = datetime.strptime(start_time, "%Y-%m-%dT%H:%M:%SZ")
     alpha_a, alpha_s, beta_v, beta_r = [0, 0, 0, 0]
     for i in range(days):
@@ -48,6 +54,7 @@ def train_for_time_frame(start_time = "2025-01-01T00:00:00Z", days = 1):
         t_r = df["room_temp"].values
         t_a = df["ambient_temp"].values
         s = df["watt"].values
+        h_sp = df["heating_setpoint"].values
         initial_guess = np.array([0.01, 0.001, 0.1, 0.1])
         result = minimize(mean_squared_error, initial_guess, method="COBYQA")
 
@@ -76,7 +83,8 @@ def temp_derivative(t, T, alpha_a, alpha_s, beta_r, beta_v):
         t_idx = len(t_a) - 1
 
     solar_impact = solar_effect(s[t_idx])
-    return (t_a[t_idx] - T) * alpha_a + solar_impact * alpha_s + beta_r + beta_v
+    heater_impact = heater_effect(h_sp[t_idx], T)
+    return (t_a[t_idx] - T) * alpha_a + solar_impact * alpha_s + heater_impact * beta_r + beta_v
 
 def predict_temperature(constants):
     alpha_a, alpha_s, beta_r, beta_v = constants
@@ -96,3 +104,12 @@ def solar_effect(df_watt):
     G = 0.7
     mean_window_area_group = 4.25
     return df_watt * G * mean_window_area_group
+
+def heater_effect(setpoint, room_temp):
+    if room_temp < setpoint:
+        return setpoint-room_temp
+    else:
+        return 1
+
+def ventilation_effect(something):
+    return 1
