27c8f2c6bfffc6115a27bd4d10a3a60b09b6524e
Verdoner <jx55oc@student.aau.dk>
2025-03-04 17:09:47 +0100

    Added testing for data_processing.py: Added __init__.py to all directories for easy transfer of modules.
    Input:
    PYTHONPATH=your/path/to/BlindControl
    in .env file for it to work

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from scripts.greybox_fitting import get_constants
 from scripts.prediction import predict_for_date
 
-constants = get_constants("2024-11-25T00:00:00Z", 2, True)
+constants = get_constants("2024-11-25T00:00:00Z", 0, True)
 print(constants)
 predict_for_date("2024-12-27T00:00:00Z", constants, True)
---
+++ tests/test_data_processing.py
@@ -0,0 +1,147 @@
+import pytest
+import pandas as pd
+import numpy as np
+from io import StringIO
+import sys
+import os
+
+
+from scripts.data_processing import normalize, remove_outliers, convert_csv_to_df, smooth
+
+
+@pytest.fixture
+def sample_data():
+    csv_data = """
+    time,solar_watt,room_temp,ambient_temp,heating_setpoint,cooling_setpoint
+    2024-12-27 09:03:00+00:00,7.5467529297,22.2,6.6,23.0,25.0
+    2024-12-27 09:18:00+00:00,7.5467529297,22.3,6.6,23.0,25.0
+    2024-12-27 09:33:00+00:00,7.5467529297,22.3,6.6,23.0,25.0
+    2024-12-27 09:48:00+00:00,11.7648925781,22.2,6.6,23.0,25.0
+    2024-12-27 10:03:00+00:00,11.7648925781,22.3,6.6,23.0,25.0
+    2024-12-27 10:18:00+00:00,13.7916259766,22.4,6.6,23.0,25.0
+    2024-12-27 10:34:00+00:00,15.8675537109,22.2,6.6,23.0,25.0
+    2024-12-27 10:49:00+00:00,24.3049316406,22.4,6.6,23.0,25.0
+    2024-12-27 11:06:00+00:00,22.1949462891,22.4,6.6,23.0,25.0
+    2024-12-27 11:21:00+00:00,20.0246582031,22.5,6.6,23.0,25.0
+    2024-12-27 11:36:00+00:00,18.0151367188,22.4,6.4,23.0,25.0
+    2024-12-27 11:51:00+00:00,13.8404541016,22.5,6.4,23.0,25.0
+    2024-12-27 12:06:00+00:00,18.1911621094,22.5,6.4,23.0,25.0
+    2024-12-27 12:21:00+00:00,16.1551513672,22.5,6.4,23.0,25.0
+    2024-12-27 12:36:00+00:00,20.3659667969,22.4,6.4,23.0,25.0
+    2024-12-27 12:51:00+00:00,18.3323974609,22.4,6.4,23.0,25.0
+    2024-12-27 13:06:00+00:00,14.1296386719,22.3,6.4,23.0,25.0
+    2024-12-27 13:21:00+00:00,10.0076904297,22.4,30,23.0,25.0
+    2024-12-27 13:36:00+00:00,10.0076904297,100,6.2,23.0,25.0
+    2024-12-27 13:51:00+00:00,150,22.4,6.2,23.0,25.0
+    2024-12-27 14:06:00+00:00,5.9259033203,22.4,6.2,23.0,25.0
+    2024-12-27 14:21:00+00:00,3.9113769531,22.4,6.2,23.0,25.0
+    2024-12-27 14:36:00+00:00,3.9113769531,22.5,6.1,23.0,25.0
+    2024-12-27 14:51:00+00:00,3.9113769531,22.4,6.1,23.0,25.0
+    2024-12-27 15:06:00+00:00,3.9113769531,22.6,6.1,23.0,25.0
+    2024-12-27 15:21:00+00:00,3.9113769531,22.6,6.1,23.0,25.0
+    2024-12-27 15:36:00+00:00,3.9113769531,22.8,6.1,23.0,25.0
+    2024-12-27 15:51:00+00:00,3.9113769531,22.9,6.1,23.0,25.0
+    2024-12-27 16:06:00+00:00,3.9113769531,22.7,6.1,22.0,26.0
+    2024-12-27 16:21:00+00:00,3.9113769531,22.8,6.1,22.0,26.0
+    2024-12-27 16:37:00+00:00,3.9113769531,22.6,6.0,22.0,26.0
+    2024-12-27 16:52:00+00:00,3.9113769531,22.7,6.0,22.0,26.0
+    2024-12-27 17:07:00+00:00,3.9113769531,22.3,6.0,22.0,26.0
+    2024-12-27 17:22:00+00:00,3.9113769531,22.3,6.0,22.0,26.0
+    2024-12-27 17:37:00+00:00,3.9113769531,22.2,5.9,22.0,26.0
+    2024-12-27 17:52:00+00:00,3.9113769531,22.2,5.9,22.0,26.0
+    2024-12-27 18:07:00+00:00,3.9113769531,22.2,5.9,22.0,26.0
+    2024-12-27 18:22:00+00:00,3.9113769531,22.0,5.9,22.0,26.0
+    2024-12-27 18:37:00+00:00,3.9113769531,22.0,5.9,22.0,26.0
+    2024-12-27 18:52:00+00:00,3.9113769531,22.0,5.9,22.0,26.0
+    2024-12-27 19:07:00+00:00,3.9113769531,22.0,5.9,22.0,26.0
+    2024-12-27 19:22:00+00:00,3.9113769531,22.0,5.9,22.0,26.0
+    2024-12-27 19:37:00+00:00,3.9113769531,22.0,5.9,22.0,26.0
+    2024-12-27 19:52:00+00:00,3.9113769531,21.9,5.9,22.0,26.0
+    2024-12-27 20:07:00+00:00,3.9113769531,22.0,5.9,22.0,26.0
+    2024-12-27 20:22:00+00:00,3.9113769531,22.4,5.9,22.0,26.0
+    2024-12-27 20:38:00+00:00,3.9113769531,22.2,5.9,22.0,26.0
+    2024-12-27 20:53:00+00:00,3.9113769531,22.0,5.9,22.0,26.0
+    2024-12-27 21:10:00+00:00,3.9113769531,22.1,5.9,22.0,26.0
+    2024-12-27 21:25:00+00:00,3.9113769531,22.0,5.9,22.0,26.0
+    2024-12-27 21:40:00+00:00,3.9113769531,22.0,5.9,22.0,26.0
+    """
+    df = pd.read_csv(StringIO(csv_data))
+    return df
+
+# Test normalize function
+def test_normalize(sample_data):
+    df = sample_data.copy()
+    normalized_df = normalize(df.copy())
+    assert 'solar_watt' in df and 'room_temp' in df and 'ambient_temp' in df
+
+    # Normalize the 'solar_watt' column
+    solar_min = df['solar_watt'].min()
+    solar_max = df['solar_watt'].max()
+    expected_solar_watt = 20 + (df['solar_watt'] - solar_min) / (solar_max - solar_min)
+
+    # Normalize the 'room_temp' column
+    temp_min = df['room_temp'].min()
+    temp_max = df['room_temp'].max()
+    expected_room_temp = 20 + (df['room_temp'] - temp_min) / (temp_max - temp_min)
+
+    # Normalize the 'ambient_temp' column
+    ambient_min = df['ambient_temp'].min()
+    ambient_max = df['ambient_temp'].max()
+    expected_ambient_temp = 20 + (df['ambient_temp'] - ambient_min) / (ambient_max - ambient_min)
+
+    # Compare the normalized values
+    try:
+        pd.testing.assert_series_equal(normalized_df['solar_watt'], expected_solar_watt)
+        pd.testing.assert_series_equal(normalized_df['room_temp'], expected_room_temp)
+        pd.testing.assert_series_equal(normalized_df['ambient_temp'], expected_ambient_temp)
+    except AssertionError:
+        raise AssertionError(f"Normalization failed")
+
+
+# Test remove_outliers function
+def test_remove_outliers(sample_data):
+    df = sample_data.copy()
+    df_cleaned = remove_outliers(df)
+    assert len(df_cleaned) < len(df), "Outlier removal did not work as expected"
+    assert df_cleaned['solar_watt'].max() <= df['solar_watt'].max(), f"Outliers detected in solar_watt after outlier removal."
+    assert df_cleaned['solar_watt'].min() >= df['solar_watt'].min(), f"Outliers detected in solar_watt after outlier removal."
+    
+    assert df_cleaned['room_temp'].max() <= df['room_temp'].max(), f"Outliers detected in room_temp after outlier removal."
+    assert df_cleaned['room_temp'].min() >= df['room_temp'].min(), f"Outliers detected in room_temp after outlier removal."
+
+    assert df_cleaned['ambient_temp'].max() <= df['ambient_temp'].max(), f"Outliers detected in ambient_temp after outlier removal."
+    assert df_cleaned['ambient_temp'].min() >= df['ambient_temp'].min(), f"Outliers detected in ambient_temp after outlier removal."
+
+# Test convert_csv_to_df function
+def test_convert_csv_to_df():
+    csv_data = """time,solar_watt,room_temp,ambient_temp,heating_setpoint,cooling_setpoint
+    2024-12-27 17:22:00+00:00,3.9113769531,22.3,6.0,22.0,26.0
+    2024-12-27 17:37:00+00:00,3.9113769531,22.2,5.9,22.0,26.0
+    2024-12-27 17:52:00+00:00,3.9113769531,22.2,5.9,22.0,26.0
+    2024-12-27 18:07:00+00:00,3.9113769531,22.2,5.9,22.0,26.0
+    2024-12-27 18:22:00+00:00,3.9113769531,22.0,5.9,22.0,26.0
+    2024-12-27 18:37:00+00:00,3.9113769531,22.0,5.9,22.0,26.0
+    2024-12-27 18:52:00+00:00,3.9113769531,22.0,5.9,22.0,26.0
+    2024-12-27 19:07:00+00:00,3.9113769531,22.0,5.9,22.0,26.0
+    2024-12-27 19:22:00+00:00,3.9113769531,22.0,5.9,22.0,26.0
+    2024-12-27 19:37:00+00:00,3.9113769531,22.0,5.9,22.0,26.0
+    2024-12-27 19:52:00+00:00,3.9113769531,21.9,5.9,22.0,26.0
+    2024-12-27 20:07:00+00:00,3.9113769531,22.0,5.9,22.0,26.0
+    2024-12-27 20:22:00+00:00,3.9113769531,22.4,5.9,22.0,26.0
+    2024-12-27 20:38:00+00:00,3.9113769531,22.2,5.9,22.0,26.0
+    2024-12-27 20:53:00+00:00,3.9113769531,22.0,5.9,22.0,26.0
+    2024-12-27 21:10:00+00:00,3.9113769531,22.1,5.9,22.0,26.0
+    2024-12-27 21:25:00+00:00,3.9113769531,22.0,5.9,22.0,26.0
+    2024-12-27 21:40:00+00:00,3.9113769531,22.0,5.9,22.0,26.0
+    """
+    df = convert_csv_to_df(StringIO(csv_data))
+    assert isinstance(df, pd.DataFrame)
+    assert not df.empty
+    assert 'time' in df.columns
+    assert df["time"].is_monotonic_increasing, "'time' column is not sorted"
+
+# Test smooth function
+def test_smooth(sample_data):
+    df_smoothed = smooth(sample_data.copy())
+    assert 'room_temp' in df_smoothed
+    assert not df_smoothed['room_temp'].equals(sample_data['room_temp']), "Smoothing did not change values"
