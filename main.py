21b1002f97e52692e37ccea0b9b0683375a60e06
EmilieSchultzzz <113088113+EmilieSchultzzz@users.noreply.github.com>
2025-04-10 12:31:25 +0200

    changed prediction interval

--- main.py
+++ main.py
@@ -68,7 +68,7 @@ def main():
     #if train:
         #constants = get_constants(chosen_room, training_start_date, training_days, force_retrain, prediction_interval)
     predict_for_date(chosen_room, predict_date, [1.99706603e-04, 1.12559767e-05, 7.83316484e-05, 2.98706584e-04,
-       2.33775480e-05], True, 60)
+       2.33775480e-05], True, 1440)
 
 if __name__ == '__main__':
     # Initialization of multiprocessing
